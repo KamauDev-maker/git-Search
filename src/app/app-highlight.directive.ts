a7c57959857747688f747bcd224a40307cc6d7fc
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-17 21:47:48 +0300

    modifying apphighlight & user result

--- src/app/app-highlight.directive.ts
+++ src/app/app-highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef,HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appAppHighlight]'
 })
 export class AppHighlightDirective {
+   
+  constructor(private elem:ElementRef) {}
 
-  constructor() { }
+  @HostListener('mouseenter') onMouseEnter(){
+    this.highlight('40DFEF');
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.highlight( 'None');
+
+  }
+
+  private highlight(color:string){
+    this.elem.nativeElement.style.backgroundColor = color;
+  }
 
 }
