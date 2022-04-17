6fe96ab6cb924b27a9129e35767fbc6094f36f87
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-17 20:13:39 +0300

    creating a http client for user service

--- src/app/gituser-service.service.ts
+++ src/app/gituser-service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { User } from './user';
+import { Repo } from './repo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GituserServiceService {
 
-  constructor() { }
+  user:User;
+  repo:Repo;
+  repoData = [];
+  newUserData: any = [];
+  showInput:boolean;
+  showData:boolean
+
+  constructor(private http: HttpClient) {
+    this.user = new User("","",new Date(),new Date(),"",0,"");
+    this.repo = new Repo ("","","","","",new Date(),new Date(),"")
+
+   }
 }
--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Output} from '@angular/core';
+import { GituserServiceService } from '../gituser-service.service';
+import {HttpClient} from '@angular/common/http';
+import { User } from '../user';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search-form',
@@ -7,7 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFormComponent implements OnInit {
 
-  constructor() { }
+  user:User;
+  username:string;
+  gitUserServiceService:GituserServiceService;
+  public showInput = true;
+  public showData = false;
+
+  submitUsername(){
+    this.gitUserServiceService.getUserData(this.username);
+    this.showInput = false;
+    this.showData = true;
+  }
+
+  showUserInput(hideInput: any){
+    this.showInput = hideInput;
+    this.showData = false;
+  }
+  
+
+
+  constructor(gitUserServiceService:GituserServiceService) { 
+    this.gitUserServiceService = gitUserServiceService;
+  }
 
   ngOnInit(): void {
   }
